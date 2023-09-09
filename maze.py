e4aaad9ba7cbb10d87a3570a30138e6661cfca5e
berkayeren <berkay-eren@hotmail.com>
2023-09-09 18:11:26 +0300

    Version downgrade for Python 3.7 compatibility

--- maze.py
+++ maze.py
@@ -1,3 +1,5 @@
 from ray.rllib.algorithms.dqn import DQNConfig
 
 config = DQNConfig().environment("classes.environment.GymEnvironment").rollouts(num_rollout_workers=2)
+
+# rllib train file maze.py --stop '{\"timesteps_total\": 10000}'
