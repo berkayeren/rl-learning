ad2cf5e6a00d599db77948168e766d16d0c58662
BerkayEren <berkay.eren@teamdefinex.com>
2024-05-06 14:24:30 +0300

    Dowham intrinsic reward fixes are made.

--- custom_env.py
+++ custom_env.py
@@ -1,5 +1,8 @@
-from minigrid.core.world_object import Door, Goal
+from __future__ import annotations
+
+from minigrid.core.world_object import Door, Goal, Key
 from minigrid.envs import MultiRoomEnv
+from minigrid.wrappers import FlatObsWrapper
 
 from dowham import DoWhaMIntrinsicReward
 
@@ -8,7 +11,7 @@ class CustomMultiRoomEnv(MultiRoomEnv):
     def __init__(self, *args, **kwargs):
         self.max_episode_steps = kwargs.pop('max_episode_steps', 100)
         self.enable_dowham = kwargs.pop('enable_dowham', False)
-        self.dowham_reward = kwargs.pop('dowham_reward', DoWhaMIntrinsicReward(eta=1.5, H=1.0, tau=0.5))
+        self.dowham_reward = kwargs.pop('dowham_reward', DoWhaMIntrinsicReward(eta=2.5, H=1, tau=0.5))
         # Extract custom configurations or set defaults
         self.previous_distance = 0
         self.custom_agent_start_pos = kwargs.pop('agent_start_pos', (1, 1))
@@ -19,6 +22,7 @@ class CustomMultiRoomEnv(MultiRoomEnv):
         self.agent_dir = self.custom_agent_start_dir
 
         super().__init__(*args, **kwargs)
+        self.carrying = Key('yellow')
 
     def _gen_grid(self, width, height):
         # Create the grid and place walls around the border
@@ -51,29 +55,107 @@ class CustomMultiRoomEnv(MultiRoomEnv):
             next_state = self.agent_pos
             self.dowham_reward.update_state_visits(next_state)
             state_changed = current_state != next_state
-            self.dowham_reward.update_usage(action)
-            self.dowham_reward.update_effectiveness(action, state_changed)
+            self.dowham_reward.update_usage(current_state, action)
+            self.dowham_reward.update_effectiveness(action, current_state, next_state, state_changed)
             intrinsic_reward = self.dowham_reward.calculate_intrinsic_reward(action, current_state, next_state)
             reward += intrinsic_reward
 
         return obs, reward, done, info, {}
 
-#
-#
-# class CustomFlatObsWrapper(FlatObsWrapper):
-#     def __init__(self, env):
-#         super().__init__(env)
-#
-#
+
+class CustomFlatObsWrapper(FlatObsWrapper):
+    def __init__(self, env):
+        super().__init__(env)
+
+
 # if __name__ == "__main__":
-#     env_config = {"render_mode": "human"}
-#     env = CustomMultiRoomEnv(agent_start_pos=(1, 1),
-#                              agent_start_dir=0,
-#                              goal_pos=(18, 18),
-#                              minNumRooms=2,
-#                              maxNumRooms=5, **env_config)
+#     from ray.rllib.algorithms import DQN
+#     from ray.rllib.algorithms.dqn import DQNConfig
+#     from custom_env import CustomMultiRoomEnv
+#
+#     try:
+#         import gymnasium as gym
+#
+#         gymnasium = True
+#     except Exception:
+#         import gym
+#
+#         gymnasium = False
+#     from ray.tune import register_env
+#
+#
+#     def env_creator(env_config=None):
+#         config = {
+#             "agent_start_pos": (1, 1),
+#             "agent_start_dir": 0,
+#             "goal_pos": (15, 15),
+#             "minNumRooms": 2,
+#             "maxNumRooms": 5,
+#             "enable_dowham": True,
+#             "max_episode_steps": 1000,
+#             **env_config
+#         }
+#         env = CustomMultiRoomEnv(**config)
+#         env.reset()
+#         env = CustomFlatObsWrapper(env)
+#         return env
+#
+#
+#     # Register the custom environment
+#     register_env("my_minigrid_env", env_creator)
+#
+#     config = {
+#         "agent_start_pos": (1, 1),
+#         "agent_start_dir": 0,
+#         "goal_pos": (15, 15),
+#         "minNumRooms": 2,
+#         "maxNumRooms": 5,
+#         "enable_dowham": True,
+#         "max_episode_steps": 1000,
+#         "render_mode": "human"
+#     }
+#     env = CustomMultiRoomEnv(**config)
 #     env.reset()
 #     env = CustomFlatObsWrapper(env)
-#     env.reset()
-#     while True:
+#     best_result_path = "C:/Users/BerkayEren/PycharmProjects/rl-learning/ray_results/checkpoints"
+#
+#     tune_config = DQNConfig().environment("my_minigrid_env").rollouts(
+#         num_envs_per_worker=20,
+#         observation_filter="MeanStdFilter",
+#         num_rollout_workers=0,
+#     ).exploration(
+#         explore=True,
+#         exploration_config={
+#             "type": "EpsilonGreedy",
+#             "initial_epsilon": 1.0,
+#             "final_epsilon": 0.1,
+#             "epsilon_timesteps": 10000,
+#         }
+#     ).training()
+#     observation, info = env.reset()
+#     done = False
+#     action = None
+#     reward = 0
+#     tune_config_dict = tune_config.to_dict()
+#     trainer = DQN(config=tune_config_dict)
+#     trainer.restore(best_result_path)
+#
+#     for trial in range(0):
+#         print(f"Running trial {trial + 1}")
+#         result = trainer.train()
+#
+#         # Save the trainer every 50 trials
+#         if (trial + 1) % 50 == 0:
+#             checkpoint_path = trainer.save(
+#                 "C:/Users/BerkayEren/PycharmProjects/rl-learning/ray_results/checkpoints")
+#             print(f"Saved checkpoint to: {checkpoint_path.checkpoint.path}")
+#
+#     while not done:
+#         # Compute the action using the trained policy
+#         action = trainer.compute_single_action(observation=observation, prev_action=action, prev_reward=reward)
+#
+#         # Take the action in the environment
+#         observation, reward, done, info, _ = env.step(action)
+#
+#         # Render the environment
 #         env.render()
--- dowham.py
+++ dowham.py
@@ -9,18 +9,39 @@ class DoWhaMIntrinsicReward:
         self.usage_counts = {}  # Tracks the usage of each action
         self.effectiveness_counts = {}  # Tracks the effectiveness of each action
         self.state_visit_counts = {}  # Tracks the number of times a state is visited in the current episode
+        self.intrinsic_rewards = {}
 
-    def update_usage(self, action):
-        self.usage_counts[action] = self.usage_counts.get(action, 0) + 1
+    def update_usage(self, state, action):
+        if state not in self.usage_counts:
+            self.usage_counts[state] = {}
 
-    def update_effectiveness(self, action, state_changed):
-        if state_changed:
-            self.effectiveness_counts[action] = self.effectiveness_counts.get(action, 0) + 1
+        if action not in self.usage_counts[state]:
+            self.usage_counts[state][action] = 0
 
-    def calculate_action_bonus(self, action):
-        U = self.usage_counts.get(action, 1)  # Avoid division by zero
-        E = self.effectiveness_counts.get(action, 0)
-        bonus = (self.eta ** (1 - (E ** self.H) / (U ** self.H)) - 1) / (self.eta - 1)
+        self.usage_counts[state][action] += 1
+
+    def update_effectiveness(self, action, state, next_state, state_changed):
+        if state not in self.effectiveness_counts:
+            self.effectiveness_counts[state] = {}
+
+        if action not in self.effectiveness_counts[state]:
+            self.effectiveness_counts[state][action] = 0
+
+        if state_changed and self.effectiveness_counts[state][action] == 0 and self.state_visit_counts.get(next_state,
+                                                                                                           0) == 0:
+            self.effectiveness_counts[state][action] = 1
+
+        if state_changed and self.effectiveness_counts[state][action] == 1:
+            self.effectiveness_counts[state][action] = 0
+
+    def calculate_action_bonus(self, state, next_state, action):
+        U = self.usage_counts[state].get(action, 0)
+        E = self.effectiveness_counts[state].get(action, 0)
+        # print(f"Usage (U): {U}, Effectiveness (E): {E}")  # Debug output
+        term = (E ** self.H) / (U ** self.H)
+        exp_term = self.eta ** (1 - term)
+        bonus = (exp_term - 1) / (self.eta - 1)
+        # print(f"Term: {term}, Exp Term: {exp_term}, Bonus: {bonus}")  # Debug output
         return bonus
 
     def update_state_visits(self, state):
@@ -29,8 +50,17 @@ class DoWhaMIntrinsicReward:
     def calculate_intrinsic_reward(self, action, state, next_state):
         if state != next_state:
             state_count = self.state_visit_counts.get(next_state, 1) ** self.tau
-            action_bonus = self.calculate_action_bonus(action)
+            action_bonus = self.calculate_action_bonus(state, next_state, action)
             intrinsic_reward = action_bonus / np.sqrt(state_count)
+            # print(f"State Count: {state_count}, Action Bonus: {action_bonus}, Intrinsic Reward: {intrinsic_reward}")
+
+            # if state not in self.intrinsic_rewards:
+            #     self.intrinsic_rewards[state] = []
+            #
+            # self.intrinsic_rewards[state].append(intrinsic_reward)
+
+            # print(self.intrinsic_rewards[state])
+
             return intrinsic_reward
         return 0.0
 
