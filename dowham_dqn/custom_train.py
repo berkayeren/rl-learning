dd7ae48a37581569fb95f7b3739e99df0176af5f
BerkayEren <berkay.eren@teamdefinex.com>
2024-08-22 23:15:34 +0300

    Improvements.

--- dowham_dqn/custom_train.py
+++ dowham_dqn/custom_train.py
@@ -24,6 +24,7 @@ from tqdm import tqdm
 from custom_dqn_model import MinigridPolicyNet
 from custom_playground_env import CustomPlaygroundEnv, MiniGridNet
 
+os.environ['PYTHONWARNINGS'] = "ignore::DeprecationWarning"
 # Initialize Ray
 ray.init(ignore_reinit_error=True, _metrics_export_port=8080)
 
@@ -46,130 +47,130 @@ parser.add_argument('--enable_prediction_reward', type=bool, help='Restore from 
 parser.add_argument('--output_folder', type=str, help='Output Folder', default="ray_results")
 parser.add_argument('--batch_size', type=int, help='Batch Size', default=32)
 
-# Parse the arguments
-args = parser.parse_args()
-
-# Get the current working directory
-current_dir = os.getcwd()
-
-# Append the output folder to the current file path
-output_folder_path = os.path.join(os.path.dirname(current_dir), args.output_folder)
-
-
-class AccuracyCallback(DefaultCallbacks):
-    training_iteration = 0  # Class-level attribute
-
-    def __init__(self, path=output_folder_path):
-        super().__init__()
-        self.states = None
-        self.height = 0
-        self.width = 0
-        self.path = path
-
-    def on_episode_start(
-            self,
-            *,
-            worker: "RolloutWorker",
-            base_env: BaseEnv,
-            policies: Dict[PolicyID, Policy],
-            episode: Union[Episode, EpisodeV2],
-            env_index: Optional[int] = None,
-            **kwargs,
-    ) -> None:
-        super().on_episode_start(worker=worker, base_env=base_env, policies=policies, episode=episode,
-                                 env_index=env_index, **kwargs)
-        self.visited_states = set()
-        self.height = base_env.get_sub_environments()[0].unwrapped.height
-        self.width = base_env.get_sub_environments()[0].unwrapped.width
-        self.states = np.full((self.width, self.height), 0)
-
-    def on_episode_step(
-            self,
-            *,
-            worker: "RolloutWorker",
-            base_env: BaseEnv,
-            policies: Optional[Dict[PolicyID, Policy]] = None,
-            episode: EpisodeV2,
-            env_index: Optional[int] = None,
-            **kwargs,
-    ) -> None:
-        env = base_env.get_sub_environments()[0].unwrapped
-        x, y = base_env.get_sub_environments()[0].unwrapped.agent_pos
-        self.states[x][y] += 1
-
-        if hasattr(env, "dowham_reward") and hasattr(env, "intrinsic_reward"):
-            episode.custom_metrics["intrinsic_reward"] = env.intrinsic_reward
-
-        if hasattr(env, "count_exploration") and hasattr(env, "count_bonus"):
-            episode.custom_metrics["count_bonus"] = env.count_bonus
-
-        episode.custom_metrics["prediction_reward"] = env.prediction_reward
-        episode.custom_metrics["prediction_prob"] = env.prediction_prob
-
-    def preprocess_observation(self, obs):
-        image = torch.tensor(obs["image"], dtype=torch.float32).permute(2, 0, 1).unsqueeze(0)
-        direction = torch.tensor([[obs["direction"]]], dtype=torch.float32)
-        return image, direction
-
-    def on_episode_end(
-            self,
-            *,
-            worker: "RolloutWorker",
-            base_env: BaseEnv,
-            policies: Dict[PolicyID, Policy],
-            episode: Union[Episode, EpisodeV2, Exception],
-            env_index: Optional[int] = None,
-            **kwargs,
-    ) -> None:
-        for env in base_env.get_sub_environments():
-            env = env.unwrapped
-            # Iterate through all collected episodes
-            for eh in env.episode_history:
-                # Extract the current observation from the episode
-                current_obs = eh["current_obs"]
-                # Extract the action taken in the episode
-                action = eh["action"]
-
-                # Preprocess the current observation to get the image and direction tensors
-                image, direction = self.preprocess_observation(current_obs)
-
-                # Zero the gradients of the prediction optimizer
-                env.prediction_optimizer.zero_grad()
-                # Perform a forward pass through the prediction network
-                outputs = env.prediction_net(image, direction)
-                # Compute the loss between the network's output and the actual action taken
-                loss = env.prediction_criterion(outputs, torch.tensor([action], dtype=torch.long))
-                # Perform a backward pass to compute the gradients
-                loss.backward()
-                # Update the model parameters using the computed gradients
-                env.prediction_optimizer.step()
-
-        torch.save({
-            'model_state_dict': env.prediction_net.state_dict(),
-            'optimizer_state_dict': env.prediction_optimizer.state_dict(),
-        },
-            f'{os.path.join(self.path, "prediction_network")}/prediction_network_checkpoint.pth')
-
-        env = base_env.get_sub_environments()[0].unwrapped
-        total_size = self.width * self.height
-        # Calculate the number of unique states visited by the agent
-        unique_states_visited = np.count_nonzero(self.states)
-
-        # Calculate the percentage of the environment the agent has visited
-        percentage_visited = (unique_states_visited / total_size) * 100
-
-        # Log the percentage
-        episode.custom_metrics["percentage_visited"] = percentage_visited
-        episode.custom_metrics["left"] = env.action_count[0]
-        episode.custom_metrics["right"] = env.action_count[1]
-        episode.custom_metrics["forward"] = env.action_count[2]
-        episode.custom_metrics["pickup"] = env.action_count[3]
-        episode.custom_metrics["drop"] = env.action_count[4]
-        episode.custom_metrics["toggle"] = env.action_count[5]
-        episode.custom_metrics["done"] = env.action_count[6]
-
-
 if __name__ == "__main__":
+    # Parse the arguments
+    args = parser.parse_args()
+
+    # Get the current working directory
+    current_dir = os.getcwd()
+
+    # Append the output folder to the current file path
+    output_folder_path = os.path.join(os.path.dirname(current_dir), args.output_folder)
+
+
+    class AccuracyCallback(DefaultCallbacks):
+        training_iteration = 0  # Class-level attribute
+
+        def __init__(self, path=output_folder_path):
+            super().__init__()
+            self.states = None
+            self.height = 0
+            self.width = 0
+            self.path = path
+
+        def on_episode_start(
+                self,
+                *,
+                worker: "RolloutWorker",
+                base_env: BaseEnv,
+                policies: Dict[PolicyID, Policy],
+                episode: Union[Episode, EpisodeV2],
+                env_index: Optional[int] = None,
+                **kwargs,
+        ) -> None:
+            super().on_episode_start(worker=worker, base_env=base_env, policies=policies, episode=episode,
+                                     env_index=env_index, **kwargs)
+            self.visited_states = set()
+            self.height = base_env.get_sub_environments()[0].unwrapped.height
+            self.width = base_env.get_sub_environments()[0].unwrapped.width
+            self.states = np.full((self.width, self.height), 0)
+
+        def on_episode_step(
+                self,
+                *,
+                worker: "RolloutWorker",
+                base_env: BaseEnv,
+                policies: Optional[Dict[PolicyID, Policy]] = None,
+                episode: EpisodeV2,
+                env_index: Optional[int] = None,
+                **kwargs,
+        ) -> None:
+            env = base_env.get_sub_environments()[0].unwrapped
+            x, y = base_env.get_sub_environments()[0].unwrapped.agent_pos
+            self.states[x][y] += 1
+
+            if hasattr(env, "dowham_reward") and hasattr(env, "intrinsic_reward"):
+                episode.custom_metrics["intrinsic_reward"] = env.intrinsic_reward
+
+            if hasattr(env, "count_exploration") and hasattr(env, "count_bonus"):
+                episode.custom_metrics["count_bonus"] = env.count_bonus
+
+            episode.custom_metrics["prediction_reward"] = env.prediction_reward
+            episode.custom_metrics["prediction_prob"] = env.prediction_prob
+
+        def preprocess_observation(self, obs):
+            image = torch.tensor(obs["image"], dtype=torch.float32).permute(2, 0, 1).unsqueeze(0)
+            direction = torch.tensor([[obs["direction"]]], dtype=torch.float32)
+            return image, direction
+
+        def on_episode_end(
+                self,
+                *,
+                worker: "RolloutWorker",
+                base_env: BaseEnv,
+                policies: Dict[PolicyID, Policy],
+                episode: Union[Episode, EpisodeV2, Exception],
+                env_index: Optional[int] = None,
+                **kwargs,
+        ) -> None:
+            for env in base_env.get_sub_environments():
+                env = env.unwrapped
+                # Iterate through all collected episodes
+                for eh in env.episode_history:
+                    # Extract the current observation from the episode
+                    current_obs = eh["current_obs"]
+                    # Extract the action taken in the episode
+                    action = eh["action"]
+
+                    # Preprocess the current observation to get the image and direction tensors
+                    image, direction = self.preprocess_observation(current_obs)
+
+                    # Zero the gradients of the prediction optimizer
+                    env.prediction_optimizer.zero_grad()
+                    # Perform a forward pass through the prediction network
+                    outputs = env.prediction_net(image, direction)
+                    # Compute the loss between the network's output and the actual action taken
+                    loss = env.prediction_criterion(outputs, torch.tensor([action], dtype=torch.long))
+                    # Perform a backward pass to compute the gradients
+                    loss.backward()
+                    # Update the model parameters using the computed gradients
+                    env.prediction_optimizer.step()
+
+            torch.save({
+                'model_state_dict': env.prediction_net.state_dict(),
+                'optimizer_state_dict': env.prediction_optimizer.state_dict(),
+            },
+                f'{os.path.join(self.path, "prediction_network")}/prediction_network_checkpoint.pth')
+
+            env = base_env.get_sub_environments()[0].unwrapped
+            total_size = self.width * self.height
+            # Calculate the number of unique states visited by the agent
+            unique_states_visited = np.count_nonzero(self.states)
+
+            # Calculate the percentage of the environment the agent has visited
+            percentage_visited = (unique_states_visited / total_size) * 100
+
+            # Log the percentage
+            episode.custom_metrics["percentage_visited"] = percentage_visited
+            episode.custom_metrics["left"] = env.action_count[0]
+            episode.custom_metrics["right"] = env.action_count[1]
+            episode.custom_metrics["forward"] = env.action_count[2]
+            episode.custom_metrics["pickup"] = env.action_count[3]
+            episode.custom_metrics["drop"] = env.action_count[4]
+            episode.custom_metrics["toggle"] = env.action_count[5]
+            episode.custom_metrics["done"] = env.action_count[6]
+
+
     # Get the current date and time
     now = datetime.datetime.now()
 
