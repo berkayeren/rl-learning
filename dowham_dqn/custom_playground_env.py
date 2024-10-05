62e4022e2cfa659c61180f093a82d8828e7a9d69
BerkayEren <berkay.eren@teamdefinex.com>
2024-10-05 13:46:27 +0300

    Hyperparameter adjustment.

--- dowham_dqn/custom_playground_env.py
+++ dowham_dqn/custom_playground_env.py
@@ -45,7 +45,7 @@ class MiniGridNet(nn.Module):
 
 
 class CustomPlaygroundEnv(MultiRoomEnv):
-    def __init__(self, intrinsic_reward_scaling=0.3, eta=40, H=1, tau=0.5, size=7, render_mode=None,
+    def __init__(self, intrinsic_reward_scaling=0.05, eta=40, H=1, tau=0.5, size=7, render_mode=None,
                  prediction_net=None,
                  prediction_criterion=None,
                  prediction_optimizer=None,
